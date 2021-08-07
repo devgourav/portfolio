2d541178b8f20895c18a76573a9822867a2e77c8
Debadutta Gourav <debadutta.gaurav@exponentia.ai>
2021-08-07 09:54:26 +0530

    added skill page

--- src/Skill.svelte
+++ src/Skill.svelte
@@ -1,7 +1,52 @@
 <script></script>
 
 <div class="skill">
-  <div class="header_text">Skills and Experience</div>
+  <div class="header_text">Skills & Experience</div>
+
+  <div class="content">
+    <div class="skill_text">
+      <p>
+        Since my beginning my journey as a developer in 2014 I have done work
+        for MNC, for startups and also worked as a freelance developer
+        collaborating with Indian as well as a Foreign clients and collegues.
+      </p>
+      <p>
+        I have a worked in various roles and worn various hats of backend,
+        frontend and fullstack developer working on languages such a Java,
+        Python, Javascript, Node.js, Angular2+ etc. Currently I am work as a
+        Senior Frontend developer.
+      </p>
+    </div>
+    <div class="skill_list">
+      <div class="skill_ele">
+        <div class="skill_name">Angular (8.5/10)</div>
+        <div class="skill_rate">
+          <progress max="100" value="85" />
+        </div>
+      </div>
+
+      <div class="skill_ele">
+        <div class="skill_name">Javascript/Typescript (8/10)</div>
+        <div class="skill_rate">
+          <progress max="100" value="80" />
+        </div>
+      </div>
+
+      <div class="skill_ele">
+        <div class="skill_name">HTML/CSS (8/10)</div>
+        <div class="skill_rate">
+          <progress max="100" value="80" />
+        </div>
+      </div>
+
+      <div class="skill_ele">
+        <div class="skill_name">Node.js (7/10)</div>
+        <div class="skill_rate">
+          <progress max="100" value="70" />
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
@@ -12,7 +57,55 @@
     padding: 4rem 0;
 
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+
+  progress {
+    width: 20rem;
+    height: 8px;
+    background-color: var(--burnt-sienna);
+    border: none;
+    border-radius: 6px;
+  }
+  progress::-webkit-progress-bar {
+    width: 20rem;
+    height: 8px;
+    background-color: var(--burnt-sienna);
+    border: none;
+    border-radius: 6px;
+  }
+  progress::-webkit-progress-value {
+    width: 20rem;
+    height: 8px;
+    background-color: var(--burnt-sienna);
+    border: none;
+    border-radius: 6px;
+  }
+  progress::-moz-progress-bar {
+    width: 20rem;
+    height: 8px;
+    background-color: var(--burnt-sienna);
+    border: none;
+    border-radius: 6px;
+  }
+
+  .skill_ele {
+    margin-bottom: 2rem;
+  }
+
+  .skill_list {
+    justify-self: end;
+    font-size: 1.2rem;
+  }
+  .content {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    justify-content: space-between;
+    grid-gap: 10px;
+    width: 900px;
+    margin-top: 3rem;
   }
 
   .header_text {
